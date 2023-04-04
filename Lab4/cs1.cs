044534c5c1047314f7d6ee87f81b8a3c7607f502
Easyvac16 <gdima9051@gmail.com>
2023-04-04 19:03:03 +0300

    Add "cs1" "cs2" moddified Program.cs

---
+++ Lab4/cs1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    internal class cs1
+    {
+        public static void task_1()
+        {
+            Console.Write("Введiть рядок чисел: ");
+            string input = Console.ReadLine();
+
+            try
+            {
+                int result = int.Parse(input);
+                Console.WriteLine($"Введене число: {result}");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Введене число не може бути представлене у форматi int.");
+            }
+            
+        }
+    }
+}
---
+++ Lab4/cs2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    internal class cs2
+    {
+        public static void task_2()
+        {
+            Console.Write("Введiть рядок з 0 i 1: ");
+            string input = Console.ReadLine();
+
+            try
+            {
+                int result = Convert.ToInt32(input, 2);
+                Console.WriteLine($"Введене число в десятковому поданнi: {result}");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Введене число не може бути представлене у форматi int.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Введений рядок мiстить некоректний формат числа.");
+            }
+        }
+    }
+}
